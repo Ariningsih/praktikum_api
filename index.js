22f2d4cb8a5df65ca3825da4af5b9e87bff8cf7d
Ariningsih <162403769+Ariningsih@users.noreply.github.com>
2025-03-21 16:39:56 +0800

    menambahkan server express dengan koneksi MySQL dan dokumentasi Swagger

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import mysql from 'mysql2';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
 
-const db = mysql.createConnection({ host: "localhost", user: "root", database: "openapi", password: ""});
+const swaggerDocument = YAML.parse(fs.readFileSync('./user-api.yml', 'utf8'));
+
+
+
+const db = mysql.createConnection({ host:"localhost", user: "root", database: "openapi", password: ""});
 const app = express();
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/users', (req, res) => {
     db.query('SELECT * FROM user', (err, results) => {
@@ -14,4 +23,5 @@ app.get('/users', (req, res) => {
         res.json(results);
     });
 });
+
 app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
