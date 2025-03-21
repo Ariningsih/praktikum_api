d417289e68eb821492373612d8844651f8946d6d
Ariningsih <162403769+Ariningsih@users.noreply.github.com>
2025-03-21 15:58:48 +0800

    Mengganti server ke express

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mysql from 'mysql2';
+
+const db = mysql.createConnection({ host: "localhost", user: "root", database: "openapi", password: ""});
+const app = express();
+
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM user', (err, results) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(results);
+    });
+});
+app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
